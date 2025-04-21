3a01b0dac476f7930cf0ba6c7b6a3b38ed7d3626
Admin <leducdientin92018@gmail.com>
2025-04-21 09:18:42 +0700

    Initial commit

---
+++ my-app/src/pages/SearchPage.jsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+    return (
+        <div className="search-page">
+            <h2>Search</h2>
+            <input type="text" placeholder="Tìm kiếm..." />
+        </div>
+    );
+}

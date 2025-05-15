9f160ba342e8855c84b62cec4a9b9474ca40b611
ToHuyTheAnh <theanhto0101@gmail.com>
2025-05-15 16:05:38 +0700

    feat: emoji chat

--- my-app/src/utils/auth.js
+++ my-app/src/utils/auth.js
@@ -3,8 +3,8 @@ import { jwtDecode } from "jwt-decode";
 export function getUserId() {
     const token = localStorage.getItem("accessToken");
     if (token) {
-    const decoded = jwtDecode(token);
-    const userId = decoded.sub;
+        const decoded = jwtDecode(token);
+        const userId = decoded.sub;
         return userId;
     }
     return null;
